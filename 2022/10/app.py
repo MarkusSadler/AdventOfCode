0baf7aee093447fb62aa98da3e986f3886c1d1c4
Markus Sadler <msadler@live.at>
2022-12-11 17:13:33 +0100

    Part One of Day 10 Done

--- 2022/10/app.py
+++ 2022/10/app.py
@@ -1,33 +1,48 @@
 CYCLE_COUNT = 0
 X = 1
+ANSWER = 0
 
-def addx(v,X, CYCLE_COUNT):
-    CYCLE_COUNT = CYCLE_COUNT + 2
+def addx(v):
+    global X
+
+    cycle()
+    cycle()
+    
     X = X + v  
     #print("addX, X=", X) 
 
 
-def noop(CYCLE_COUNT):
-    CYCLE_COUNT = CYCLE_COUNT +1
+def noop():
+    cycle()
     #print("noop executed")
 
-
-
-
-input = open('.\\2022\\10\\input.txt', "r")
-
-for line in input:
-    #print(line)
-    if "addx" in line:
-        v = line.split(" ")[1]
-        addx(int(v.strip()),X, CYCLE_COUNT)
-    else: 
-        noop(CYCLE_COUNT)
+def cycle():
+    global CYCLE_COUNT
+    global X
+    global ANSWER
     
-    if CYCLE_COUNT in (20, 60, 100, 140, 180,220):
-        print (CYCLE_COUNT, " : ", X)
+    CYCLE_COUNT = CYCLE_COUNT + 1
 
-input.close()
+    print(CYCLE_COUNT, " : ", X)
+    if CYCLE_COUNT in (20,60,100,140,180,220):
+        ANSWER = ANSWER + X * CYCLE_COUNT
+
+
+def main():
+    input = open('.\\2022\\10\\input.txt', "r")
+    for line in input:
+        #print(line)
+        if "addx" in line:
+            v = line.split(" ")[1]
+            addx(int(v.strip()))
+        else: 
+            noop()
+    print("Answer: ", ANSWER)
+    input.close()
+
+
+if __name__ == "__main__":
+    main()
 
 
 
