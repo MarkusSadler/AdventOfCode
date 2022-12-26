950a9c5d898c5a34d8eb1b89fada3fe4fd1ef885
Markus Sadler <msadler@live.at>
2022-12-26 13:05:27 +0100

    part 2 of day 3 done

--- 2022/3/app.py
+++ 2022/3/app.py
@@ -2,6 +2,7 @@ import string
 
 def main():
     global PRIORTY_LowerCase, PRIORTY_UpperCase
+    groups = []
     rucksacks = []
     input = open('.\\2022\\3\\input.txt', "r")
 
@@ -12,26 +13,43 @@ def main():
     for x in PRIORTY_UpperCase:
         PRIORITIES[x] = PRIORTY_UpperCase[x] + 26
 
+    group = []
     for line in input:
         firstpart, secondpart = line[:len(line)//2], line[len(line)//2:]
         rucksack = [firstpart.strip(),secondpart.strip()]
         rucksacks.append(rucksack)
-    
+        
+        group.append(line.strip())
+        if len(group) == 3:
+            groups.append(group)
+            group = []
+
+    print(groups)
 
     itemTypes = list(string.ascii_lowercase) + list(string.ascii_uppercase)
     doubleItems = []
-    
+
     for rucksack in rucksacks:
         for item in rucksack[0]:
             if item in rucksack[1]:
                 doubleItems.append(item)
                 break
 
+    # sumPriorities = 0
+    # for item in doubleItems:
+    #     sumPriorities = sumPriorities + PRIORITIES[item]
+
+    itemBadges = []
+    for group in groups:
+            for item in group[0]:
+                if item in group[1] and item in group[2]:
+                    itemBadges.append(item)
+                    break
+    
     sumPriorities = 0
-    for item in doubleItems:
+    for item in itemBadges:
         sumPriorities = sumPriorities + PRIORITIES[item]
-
-
+    
     print(sumPriorities)
             
 
