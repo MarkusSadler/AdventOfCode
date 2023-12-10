951c8f23535f622bd2a528b3929989a4084b31ae
Markus Sadler <msadler@live.at>
2023-12-10 16:49:26 +0100

    day 1 part 2 done!

--- 2023/1/app.py
+++ 2023/1/app.py
@@ -11,20 +11,36 @@ def main():
         firstDigit = 0
         lastDigit = 0
         
-        numbers = re.split(r'(\d+|\b(one|two|three|four|five|six|seven|eight|nine|ten)\b)', line)
+        line = line.replace("twone", "two one")
+        line = line.replace("sevenine", "seven nine")
+        line = line.replace("eightwo", "eight two")
+        line = line.replace("fiveight", "five eight")
+        line = line.replace("oneight", "one eight")
+        line = line.replace("nineight", "nine eight")
+
+        numbers = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
         
         if numbers[0].isdigit():
             firstDigit = int(numbers[0])
         else:
             firstDigit = writtenNumberToDigit(numbers[0])
         
-        if numbers[-1][0].isdigit():
+        
+        if len(numbers) == 1:
+            lastDigit = firstDigit
+        elif numbers[-1].isdigit() and len(numbers) > 1:
             lastDigit = int(numbers[-1])
         else:
             lastDigit = writtenNumberToDigit(numbers[-1])
-        print(firstDigit, lastDigit)
 
+        if firstDigit == None or lastDigit == None:
+            raise Exception("Digit not found")
+
+            
+        print(firstDigit, lastDigit)
+        totalNumber += firstDigit * 10  + lastDigit
     f.close()
+    print(totalNumber)
     
 def writtenNumberToDigit(x):
     if x == "one":
@@ -45,39 +61,12 @@ def writtenNumberToDigit(x):
         return 8
     elif x == "nine":
         return 9
-    elif x == "ten":
-        return 10
     else:
-        return 0
+        return None
 
 if __name__ == "__main__":
     main()
 
 
-def writtenNumberToDigit(x):
-    if x == "one":
-        return 1
-    elif x == "two":
-        return 2
-    elif x == "three":
-        return 3
-    elif x == "four":
-        return 4
-    elif x == "five":
-        return 5
-    elif x == "six":
-        return 6
-    elif x == "seven":
-        return 7
-    elif x == "eight":
-        return 8
-    elif x == "nine":
-        return 9
-    elif x == "ten":
-        return 10
-
-
-if __name__ == "__main__":
-    main()
 
 
